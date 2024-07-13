0fceda9a908e240e1e594d5f2dc9c7753f400518
PauloCampos97 <paulocrz97@gmail.com>
2024-07-13 19:01:49 -0300

    Logo e Calendario
    Mudei a logo da e fiz o calendário  funcionar na pagina home

--- front-react/src/app/page.tsx
+++ front-react/src/app/page.tsx
@@ -76,7 +76,7 @@ export default function Home() {
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
         <div className="flex flex-col items-center gap-3">
-          <img src="/logo.svg" alt="plann.er" />
+          <img src="/OrbiTech-Golden-Logo.png" alt="plann.er" />
           <p className="text-zinc-300 text-lg">
             Convide seus amigos e planeje sua próxima viagem!
           </p>
--- front-react/src/app/steps/destination-and-date-step.tsx
+++ front-react/src/app/steps/destination-and-date-step.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/button";
-import { ArrowRight, Calendar, MapPin, Settings2 } from "lucide-react";
+import { ArrowRight, Calendar, MapPin, Settings2, X } from "lucide-react";
+import { useState } from "react";
+import { DateRange, DayPicker } from "react-day-picker";
 
 interface DestinationAndDateStepProps {
     isGuestsInputOpen: boolean
@@ -14,6 +16,17 @@ export function DestinationAndDateStep({
     openGuestsInput,
 }: DestinationAndDateStepProps) {
 
+    const [isDatePickerOpoen, setIsDatePickerOpoen] = useState(false)
+    const [eventStartAndEndDates, setEventStartAndEndDates] = useState<DateRange | undefined>()
+
+    function openDatePicker() {
+        return setIsDatePickerOpoen(true)
+    }
+
+    function closeDatePicker() {
+        return setIsDatePickerOpoen(false)
+    }
+
     return (
         <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
             <div className="flex items-center gap-2 flex-1">
@@ -26,15 +39,28 @@ export function DestinationAndDateStep({
                 />
             </div>
 
-            <div className="flex items-center gap-2">
+            <button onClick={openDatePicker} disabled={isGuestsInputOpen} className="flex items-center gap-2 text-left">
                 <Calendar className="size-5 text-zinc-400" />
-                <input
-                    disabled={isGuestsInputOpen}
-                    type="text"
-                    placeholder="Quando?"
-                    className="bg-transparent text-zinc-100 placeholder-zinc-400 w-40 outline-none"
-                />
-            </div>
+                <span>
+                    Quando?
+                </span>
+            </button>
+
+            {isDatePickerOpoen && (
+                <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+                    <div className="rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+                        <div className="space-y-2">
+                            <div className="flex items-center justify-between">
+                                <h2 className="text-zinc-100 font-semibold">Selecione a data</h2>
+                                <button type="button" onClick={closeDatePicker}>
+                                    <X className="size-5 text-zinc-400" />
+                                </button>
+                            </div>
+                        </div>
+                        <DayPicker mode="range" selected={eventStartAndEndDates} onSelect={setEventStartAndEndDates} />
+                    </div>
+                </div>
+            )}
 
             <div className="w-px h-6 bg-zinc-800" />
 
